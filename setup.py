caaccc252bb102fd8598fcc1b76c04d1aebc8740
Sam Morrison <sam@vpac.org>
2011-03-23 16:14:12 +0800

    Added initial code

---
+++ setup.py
@@ -0,0 +1,65 @@
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin": 
+    cmdclasses = {'install_data': osx_install_data} 
+else: 
+    cmdclasses = {'install_data': install_data} 
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+data_files = []
+
+data_files.append(
+    ('/usr/sbin', ['sbin/kg-send-pbs-logs'])
+)
+
+data_files.append(
+    ('/etc/karaage', ['etc/pbs-logger.cfg'])
+)
+
+
+setup(
+    name = "karaage-pbs-logger",
+    version = '0.1',
+    url = 'http://code.vpac.org/trac/karaage/',
+    author = 'Sam Morrison',
+    author_email = 'sam@vpac.org',
+    description = 'PBS log sender for Karaage',
+    cmdclass = cmdclasses,
+    data_files = data_files,
+)
